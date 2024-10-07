7b911c4c1bd86d0897818819db86324a798949b3
sondeptrai81 <22668921.son@student.iuh.edu.vn>
2024-10-07 16:46:31 +0700

    second commit

---
+++ src/entity/NhanVien.java
@@ -0,0 +1,17 @@
+package entity;
+
+public class NhanVien {
+    private String tenNV;
+
+    public String getTenNV() {
+        return tenNV;
+    }
+
+    public void setTenNV(String tenNV) {
+        this.tenNV = tenNV;
+    }
+
+    public NhanVien(String tenNV) {
+        this.tenNV = tenNV;
+    }
+}
